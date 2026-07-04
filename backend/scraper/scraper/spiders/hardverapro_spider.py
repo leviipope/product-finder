f94f42d8b8050045293b1adae97f43281a3df147
leviipope <leventep71@gmail.com>
2026-07-04 18:17:07 +0200

    test: counter should reset in next bot commit

--- backend/scraper/scraper/spiders/hardverapro_spider.py
+++ backend/scraper/scraper/spiders/hardverapro_spider.py
@@ -24,6 +24,10 @@ class HardverSpider(scrapy.Spider):
         # 2 result listing site
         "https://hardverapro.hu/aprok/hardver/videokartya/nvidia/geforce_16xx/keres.php?stext=&stcid_text=&stcid=&stmid_text=&stmid=&minprice=50000&maxprice=90000&cmpid_text=&cmpid=&usrid_text=&usrid=&__buying=1&__buying=0&stext_none=&__brandnew=1&__brandnew=0",
 
+        # another listing site (1 result) for counter reset test
+        "https://hardverapro.hu/aprok/hardver/videokartya/nvidia/geforce_16xx/keres.php?stext=&stcid_text=&stcid=&stmid_text=&stmid=&minprice=45000&maxprice=48000&cmpid_text=&cmpid=&usrid_text=&usrid=&__buying=1&__buying=0&stext_none=&__brandnew=1&__brandnew=0",
+        
+        
         ]
     
 
